d34c70c0b2f6c92ec16c6b6e590b0902caebe0be
Keita Iisaka <iisak8a@gmail.com>
2022-06-28 19:53:25 +0100

    scroll margin fixed

--- src/assets/js/scroll.js
+++ src/assets/js/scroll.js
@@ -4,25 +4,31 @@ const deepEl = document.getElementById("deep");
 const maintenanceEl = document.getElementById("maintenance");
 const galleryEl = document.getElementById("gallery");
 const testimonialEl = document.getElementById("testimonial");
-
 let lastScrollY = window.scrollY;
+
 window.addEventListener("scroll", () => {
   if (lastScrollY < window.scrollY) {
     // goin down
     navEl.classList.add("nav--hidden");
-    // regularEl.classList.toggle("nav--scroll-margin");
-    // deepEl.classList.toggle("nav--scroll-margin");
-    // maintenanceEl.classList.toggle("nav--scroll-margin");
-    // galleryEl.classList.toggle("nav--scroll-margin");
-    // testimonialEl.classList.toggle("nav--scroll-margin");
+    regularEl.style.scrollMarginTop = "0rem";
+    deepEl.style.scrollMarginTop = "0rem";
+    maintenanceEl.style.scrollMarginTop = "0rem";
+    galleryEl.style.scrollMarginTop = "0rem";
+    testimonialEl.style.scrollMarginTop = "0rem";
   } else {
     //going up
     navEl.classList.remove("nav--hidden");
-    regularEl.classList.toggle("nav--scroll-margin");
-    deepEl.classList.toggle("nav--scroll-margin");
-    maintenanceEl.classList.toggle("nav--scroll-margin");
-    galleryEl.classList.toggle("nav--scroll-margin");
-    testimonialEl.classList.toggle("nav--scroll-margin");
+    regularEl.style.scrollMarginTop = "5.5rem";
+    deepEl.style.scrollMarginTop = "5.5rem";
+    maintenanceEl.style.scrollMarginTop = "5.5rem";
+    galleryEl.style.scrollMarginTop = "5.5rem";
+    testimonialEl.style.scrollMarginTop = "5.5rem";
+    // regularEl.classList.add("nav--scroll-margin");
+    // deepEl.classList.add("nav--scroll-margin");
+    // maintenanceEl.classList.add("nav--scroll-margin");
+    // galleryEl.classList.add("nav--scroll-margin");
+    // testimonialEl.classList.add("nav--scroll-margin");
   }
+
   lastScrollY = window.scrollY;
 });
