076da8951e95d347d8972f42e13f5c5eb0b6f2e1
Keita Iisaka <iisak8a@gmail.com>
2022-07-14 02:43:30 +0100

    cleaned up scroll.js

--- src/assets/js/scroll.js
+++ src/assets/js/scroll.js
@@ -4,15 +4,6 @@ const navLinks = document.querySelectorAll(".clickToClose");
 
 let lastScrollY = window.scrollY;
 
-// loop through links to close with a delay to counteract the scroll up
-navLinks.forEach(i => {
-  i.addEventListener("click", () => {
-    setTimeout(() => {
-      navEl.classList.add("nav--hidden");
-    }, 1000);
-  });
-});
-
 // Hide and show nav on scroll
 window.addEventListener("scroll", () => {
   if (lastScrollY < window.scrollY) {
@@ -27,27 +18,11 @@ window.addEventListener("scroll", () => {
   lastScrollY = window.scrollY;
 });
 
-// form submission validation
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  "use strict";
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll(".needs-validation");
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms).forEach(function (form) {
-    form.addEventListener(
-      "submit",
-      function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-
-        form.classList.add("was-validated");
-      },
-      false
-    );
+// loop through links to close with a delay to counteract the scroll up
+navLinks.forEach(i => {
+  i.addEventListener("click", () => {
+    setTimeout(() => {
+      navEl.classList.add("nav--hidden");
+    }, 1000);
   });
-})();
+});
